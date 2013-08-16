e6bf2f731047e1a63e82f2fedf2e9d67627bba03
Kakarotvg <jmcomega@gmail.com>
2013-08-16 07:25:26 -0700

    fixed computer block not rotating

--- Common/kakarotvg/omega/render/TileEntityComputerRenderer.java
+++ Common/kakarotvg/omega/render/TileEntityComputerRenderer.java
@@ -24,26 +24,19 @@ public class TileEntityComputerRenderer extends TileEntitySpecialRenderer {
         this.model = new ModelComputer();
     }
 
-    private void adjustRotatePivotViaMeta(World world, int x, int y, int z) {
-        int meta = world.getBlockMetadata(x, y, z);
-        GL11.glPushMatrix();
-        GL11.glRotatef(meta * (-90), 0.0F, 0.0F, 1.0F);
-        GL11.glPopMatrix();
-    }
-
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float scale) {
         //The PushMatrix tells the renderer to "start" doing something.
         GL11.glPushMatrix();
         //This is setting the initial location.
         GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
-        Minecraft.getMinecraft().renderEngine.func_110577_a(resourceloc);
         //This is the texture of your block. It's pathed to be the same place as your other blocks here.
         //This rotation part is very important! Without it, your model will render upside-down! And for some reason you DO need PushMatrix again!                       
         GL11.glPushMatrix();
         GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
+
+        adjustLightFixture(te.worldObj, te.xCoord, te.yCoord, te.zCoord, te.blockType);
         //A reference to your Model file. Again, very important.
-        this.model.render((Entity) null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
         //Tell it to stop rendering for both the PushMatrix's
         GL11.glPopMatrix();
         GL11.glPopMatrix();
@@ -58,6 +51,19 @@ public class TileEntityComputerRenderer extends TileEntitySpecialRenderer {
         int divModifier = skyLight / 65536;
         tess.setColorOpaque_F(brightness, brightness, brightness);
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) modulousModifier, divModifier);
+
+        int dir = world.getBlockMetadata(i, j, k);
+
+        GL11.glPushMatrix();
+        //This line actually rotates the renderer.
+        GL11.glRotatef(dir * (90F), 0F, 1F, 0F);
+        Minecraft.getMinecraft().renderEngine.func_110577_a(resourceloc);
+        this.model.render((Entity) null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
+        /*
+         * Place your rendering code here.
+         */
+
+        GL11.glPopMatrix();
 
     }
 }
