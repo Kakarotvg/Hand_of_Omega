f61b4271179bd7f13e0ae82596d76805313f419d
Kakarotvg <jmcomega@gmail.com>
2013-08-31 16:37:27 -0700

    Update Reference.java

--- Common/kakarotvg/omega/Reference.java
+++ Common/kakarotvg/omega/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
     public static final String MOD_ID = "handofomega";
     public static final String MOD_N = "Hand of Omega";
     public static final String MOD_V = "1.1.2_Alpha";
-
+    public static final String channels = "Omega";
 }
